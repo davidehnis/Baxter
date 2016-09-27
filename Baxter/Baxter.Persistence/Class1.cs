fe06917e7bfad8c571d3a877c668fc2c3356541a
davidehnis <david.ehnis@outlook.com>
2016-09-26 23:03:05 -0400

    single

--- Baxter/Baxter.Persistence/Class1.cs
+++ Baxter/Baxter.Persistence/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -7,13 +8,51 @@ using System.Threading.Tasks;
 
 namespace Baxter.Persistence
 {
-    public class Inclass
+    public sealed class Inclass
     {
+        private static Inclass instance = null;
+        private static readonly object padlock = new object();
+
+        Inclass()
+        {
+        }
+
+        public static Inclass Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Inclass();
+                    }
+                    return instance;
+                }
+            }
+        }
+
         public void Go()
         {
-            var connection = new SQLiteConnection("Data Source =:memory:; Version = 3; New = True; DateTimeFormat = Ticks");
-
-            connection.
+            Connection = new SQLiteConnection("Data Source =:memory:; Version = 3; New = True; DateTimeFormat = Ticks");
+            Connection.Open();
         }
+
+        public IDbConnection Connection { get; set; }
+
+        public void CreateTable()
+        {
+            string sql = "create table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 9001)";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+        }
+
     }
 }
