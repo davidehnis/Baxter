da6cd3faa95b86a8c7feb3be5f82ef47cb98d9e4
davidehnis <david.ehnis@outlook.com>
2016-09-26 22:46:51 -0400

    sqllite

---
+++ Baxter/Baxter.Persistence/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baxter.Persistence
+{
+    public class Inclass
+    {
+        public void Go()
+        {
+            var connection = new SQLiteConnection("Data Source =:memory:; Version = 3; New = True; DateTimeFormat = Ticks");
+
+            connection.
+        }
+    }
+}
